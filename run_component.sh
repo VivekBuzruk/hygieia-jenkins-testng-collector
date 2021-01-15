88576efdd06e897db8ae2c65cbf2754442a10e67
Vivek Buzruk <vivek.buzruk@gmail.com>
2021-01-15 15:31:23 +0530

    for Local build and execution

---
+++ run_component.sh
@@ -0,0 +1,2 @@
+java -jar jenkins-cucumber-test-collector.jar --spring.config.name=cucumber --spring.config.location=cucumber.properties  -Djasypt.encryptor.password=hygieiasecret
+# java -jar jenkins-cucumber-test-collector.jar --spring.config.name=cucumber --spring.config.location=/opt/hygieia/collectors/hygieia-jenkins-cucumber-collector/cucumber.properties  -Djasypt.encryptor.password=hygieiasecret
